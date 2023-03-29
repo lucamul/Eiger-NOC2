a9c5910899a57960de3b179d02622237a3bfab64
lucamul <lcmultazzu@gmail.com>
2023-03-29 10:13:39 +0200

    added new exp

--- kaiju/experiment/experiments.py
+++ kaiju/experiment/experiments.py
@@ -21,6 +21,29 @@ default = {
                     "check_commit_delays" : [-1],
 }
 
+freshness = {
+    "serversList" : [(8,8)],
+                    "txnlen" : [5],
+                    "threads" : [32],
+                    "numseconds" : 60,
+                    "configs" : [
+                                  "EIGER",
+                                  "EIGER_PORT",
+                                  "EIGER_PORT_PLUS",
+                                  "EIGER_PORT_PLUS_PLUS"
+                                ],
+                    "readprop" : [.9],
+                    "iterations" : range(0,1),
+                    "freshness" : 1,
+                    "numkeys" : [1000000],
+                    "valuesize" : [1],
+                    "keydistribution" : ["zipfian"],
+                    "bootstrap_time_ms" : 10000,
+                    "launch_in_bg" : False,
+                    "drop_commit_pcts" : [0],
+                    "check_commit_delays" : [-1],
+}
+
 num_clients = {
     "serversList" : [(8,8)],
                     "txnlen" : [5],
@@ -72,5 +95,6 @@ num_servers = {
 experiments = { 
                 "default" : default,
                 "num_clients" : num_clients,
-                "num_servers" : num_servers,               
+                "num_servers" : num_servers,
+                "freshness" : freshness,               
 }
