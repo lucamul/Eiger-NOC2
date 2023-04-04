78bb85176981f0be05d02f6c69bb7ca19159f4d6
lucamul <lcmultazzu@gmail.com>
2023-04-04 12:01:24 +0200

    added new scripts

--- kaiju/experiment/experiments.py
+++ kaiju/experiment/experiments.py
@@ -21,6 +21,75 @@ default = {
                     "check_commit_delays" : [-1],
 }
 
+read_prop = {
+    "serversList" : [(8,8)],
+                    "txnlen" : [5],
+                    "threads" : [32],
+                    "numseconds" : 60,
+                    "configs" : [
+                                  "EIGER",
+                                  "EIGER_PORT",
+                                  "EIGER_PORT_PLUS",
+                                  "EIGER_PORT_PLUS_PLUS"
+                                ],
+                    "readprop" : [0,0.3,0.5,0.7,.9,1],
+                    "iterations" : range(0,1),
+                    "freshness" : 0,
+                    "numkeys" : [1000000],
+                    "valuesize" : [1],
+                    "keydistribution" : ["zipfian"],
+                    "bootstrap_time_ms" : 10000,
+                    "launch_in_bg" : False,
+                    "drop_commit_pcts" : [0],
+                    "check_commit_delays" : [-1],
+}
+
+value_size = {
+    "serversList" : [(8,8)],
+                    "txnlen" : [5],
+                    "threads" : [32],
+                    "numseconds" : 60,
+                    "configs" : [
+                                  "EIGER",
+                                  "EIGER_PORT",
+                                  "EIGER_PORT_PLUS",
+                                  "EIGER_PORT_PLUS_PLUS"
+                                ],
+                    "readprop" : [.9],
+                    "iterations" : range(0,1),
+                    "freshness" : 0,
+                    "numkeys" : [1000000],
+                    "valuesize" : [1,10,100,1000,10000],
+                    "keydistribution" : ["zipfian"],
+                    "bootstrap_time_ms" : 10000,
+                    "launch_in_bg" : False,
+                    "drop_commit_pcts" : [0],
+                    "check_commit_delays" : [-1],
+}
+
+txn_len = {
+    "serversList" : [(8,8)],
+                    "txnlen" : [1,2,4,8,16,32,64,128],
+                    "threads" : [32],
+                    "numseconds" : 60,
+                    "configs" : [
+                                  "EIGER",
+                                  "EIGER_PORT",
+                                  "EIGER_PORT_PLUS",
+                                  "EIGER_PORT_PLUS_PLUS"
+                                ],
+                    "readprop" : [.9],
+                    "iterations" : range(0,1),
+                    "freshness" : 0,
+                    "numkeys" : [1000000],
+                    "valuesize" : [1],
+                    "keydistribution" : ["zipfian"],
+                    "bootstrap_time_ms" : 10000,
+                    "launch_in_bg" : False,
+                    "drop_commit_pcts" : [0],
+                    "check_commit_delays" : [-1],
+}
+
 freshness = {
     "serversList" : [(8,8)],
                     "txnlen" : [5],
@@ -96,5 +165,8 @@ experiments = {
                 "default" : default,
                 "num_clients" : num_clients,
                 "num_servers" : num_servers,
-                "freshness" : freshness,               
+                "freshness" : freshness,     
+                "txn_len" : txn_len,
+                "value_size" : value_size,
+                "read_prop" : read_prop,          
 }
