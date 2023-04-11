f8fafad0f9fed8802953bcec5b7e6b498ee09e3f
lucamul <lcmultazzu@gmail.com>
2023-04-11 07:32:00 +0200

    bug fixed in change zipf script

--- kaiju/experiment/change_zipf.sh
+++ kaiju/experiment/change_zipf.sh
@@ -7,9 +7,9 @@ fi
 
 new_value="$1"
 # replace ZIPFIAN_CONSTANT value in ScrambledZipfianGenerator.java file
-sed -i "s/ZIPFIAN_CONSTANT=[0-9]*\.[0-9]*/ZIPFIAN_CONSTANT=$new_value/" /home/ubuntu/kaiju/contrib/YCSB/core/src/main/java/com/yahoo/ycsb/generator/ZipfianGenerator.java
+sed -i "s/ZIPFIAN_CONSTANT=[0-9]*\.?[0-9]*/ZIPFIAN_CONSTANT=$new_value/" /home/ubuntu/kaiju/contrib/YCSB/core/src/main/java/com/yahoo/ycsb/generator/ZipfianGenerator.java
 
 # replace USED_ZIPFIAN_CONSTANT value in ZipfianGenerator.java file
-sed -i "s/USED_ZIPFIAN_CONSTANT=[0-9]*\.[0-9]*/USED_ZIPFIAN_CONSTANT=$new_value/" /home/ubuntu/kaiju/contrib/YCSB/core/src/main/java/com/yahoo/ycsb/generator/ScrambledZipfianGenerator.java
+sed -i "s/USED_ZIPFIAN_CONSTANT=[0-9]*\.?[0-9]*/USED_ZIPFIAN_CONSTANT=$new_value/" /home/ubuntu/kaiju/contrib/YCSB/core/src/main/java/com/yahoo/ycsb/generator/ScrambledZipfianGenerator.java
 
 echo "Constant values updated successfully!"
